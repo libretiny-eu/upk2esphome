41887932144b56966ce3f08d35e783e9e492ca72
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2023-04-21 21:43:46 +0200

    Strip newline at the end of input

--- upk2esphome/input.py
+++ upk2esphome/input.py
@@ -12,7 +12,8 @@ def parse_user_param_key(value: str) -> dict | None:
     return value
 
 
-def parse_input(input: str) -> dict:
+def parse_input(data: str) -> dict:
+    data = data.strip()
     loaders = [
         json.loads,
         parse_user_param_key,
@@ -21,7 +22,7 @@ def parse_input(input: str) -> dict:
     value: dict | None = None
     for loader in loaders:
         try:
-            value = loader(input)
+            value = loader(data)
             break
         except Exception:
             pass
