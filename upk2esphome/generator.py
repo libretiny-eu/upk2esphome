8ad07f5363d0e4f975cc3c2b7a5c506db65cff10
Peter van Dijk <peter@7bits.nl>
2025-02-08 00:21:32 +0100

    add key support

--- upk2esphome/generator.py
+++ upk2esphome/generator.py
@@ -73,9 +73,9 @@ def upk2esphome(
     opts: Opts,
     raw_extras: Union[str, dict] = None,
 ) -> YamlResult:
-    from .parts import bulb, module, monitor, netled, remote, static, switch, tuyamcu
+    from .parts import bulb, key, module, monitor, netled, remote, static, switch, tuyamcu
 
-    parts = [module, static, bulb, switch, netled, remote, monitor, tuyamcu]
+    parts = [module, static, bulb, switch, netled, remote, monitor, tuyamcu, key]
     yr = YamlResult()
 
     try:
